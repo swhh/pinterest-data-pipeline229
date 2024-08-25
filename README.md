b9357a4be480648671b18ffe546953b7b8809836
swhh <seamus.holland1@gmail.com>
2024-08-25 18:40:16 +0100

    tidy up files again

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Project Description
 
-The purpose of this project is to create build a data pipeline with Python, Kafka, Databricks and AWS services - AWS API Gateway, AWS Kinesis, AWS RDS, AWS S3, AWS AWS MKSS and AWS MAA - that is similar to the pipeline used by Pinterest to process billions of data points using batch and streaming processing.
+The purpose of this project is to create build a data pipeline with Python, Kafka, Databricks and AWS services - AWS API Gateway, AWS Kinesis, AWS RDS, AWS S3, AWS MKSS and AWS MAA - that is similar to the pipeline used by Pinterest to process billions of data points using batch and stream processing.
 
 The code streams data via APIs built with AWS API Gateway to Kafka and AWS Kinesis; the stored records are then fetched by Databricks notebooks where the data is cleaned and processed for analysis and storage purposes. 
 
@@ -10,7 +10,10 @@ The code streams data via APIs built with AWS API Gateway to Kafka and AWS Kines
 
 - user_posting_emulation Python file to fetch data from AWS RDS and stream the records to three Kafka topics via a REST proxy built with Confluent and the AWS API Gateway
 - requirements.txt detailing Python dependencies
-- data_pipeline Databricks notebook, which processes Pinterest records with Pyspark on a Databricks clusters, cleaning the records and generating analytics insights from them
+- data_pipeline Databricks notebook, which processes Pinterest records with PySpark on a Databricks cluster, cleaning the records and generating analytics insights from them
 - 0afff2eeb7e3_dag Python file which runs the above notebook on a daily schedule via AWS MWAA
 - user_posting_emulation_streaming Python file to fetch data from AWS RDS and simulate a stream of data being sent to AWS Kinesis via an AWS Gateway API
 - kinesis_pipeline is a  Databricks notebook, which fetches streaming data from AWS Kinesis, cleans the data and then stores it in delta tables. 
+
+## Usage Instructions
+
--- user_posting_emulation.py
+++ user_posting_emulation.py
@@ -3,42 +3,27 @@ import json
 import random
 import requests
 from time import sleep
-import sqlalchemy
 from sqlalchemy import text
-import yaml
+from db_connection import AWSDBConnector
 
-
-DB_CREDS = 'db_creds.yaml'
 USER_ID = '0afff2eeb7e3'
 INVOKE_URL = 'https://46wmghohz3.execute-api.us-east-1.amazonaws.com/dev/topics/{topic_name}'
 TOPIC_NAMES = [f'{USER_ID}.{topic_end}' for topic_end in ['pin', 'geo', 'user']]
+TABLE_NAMES = [f'{table_affix}_data' for table_affix in ['pinterest', 'geolocation', 'user']]
 HEADERS = {'Content-Type': 'application/vnd.kafka.json.v2+json'}
 random.seed(100)
 
 
-def read_db_creds(yaml_file: str):
-    """Read dc_creds.yaml and return creds dictionary"""
-    with open(yaml_file) as f:
-        creds_dict = yaml.safe_load(f)
-    return creds_dict
-
-
-class AWSDBConnector:
-    """AWS RDS database connection class"""
-    def __init__(self):
-        creds_dict = read_db_creds(DB_CREDS)
-        self.HOST = creds_dict['HOST']
-        self.USER = creds_dict['USER']
-        self.PASSWORD = creds_dict['PASSWORD']
-        self.DATABASE = 'pinterest_data'
-        self.PORT = 3306
-        
-    def create_db_connector(self):
-        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
-        return engine
-
-
 new_connector = AWSDBConnector()
+
+
+def generate_row(connection, table, random_row):
+    """Fetch randomrow from table with db connection and convert to JSON dict"""
+    string = text(f"SELECT * FROM {table} LIMIT {random_row}, 1")
+    selected_row = connection.execute(string)           
+    for row in selected_row:
+        print(dict(row._mapping))
+        return dict(row._mapping)
 
 
 def run_infinite_post_data_loop():
@@ -49,39 +34,19 @@ def run_infinite_post_data_loop():
         engine = new_connector.create_db_connector()
 
         with engine.connect() as connection:
-
-            pin_string = text(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
-            pin_selected_row = connection.execute(pin_string)
-            
-            for row in pin_selected_row:
-                pin_result = dict(row._mapping)
-
-            geo_string = text(f"SELECT * FROM geolocation_data LIMIT {random_row}, 1")
-            geo_selected_row = connection.execute(geo_string)
-            
-            for row in geo_selected_row:
-                geo_result = dict(row._mapping)
-
-            user_string = text(f"SELECT * FROM user_data LIMIT {random_row}, 1")
-            user_selected_row = connection.execute(user_string)
-            
-            for row in user_selected_row:
-                user_result = dict(row._mapping)
-            
-            yield pin_result, geo_result, user_result
-
+            results = (generate_row(connection, table, random_row) for table in TABLE_NAMES)          
+            yield results
 
 
 def post_record_to_kafka(data, topic_name):
     """Posts Pinterest record represented as data to Kafka topic topic_name"""
     payload = json.dumps({
     "records": [
-        {
-        #Data should be send as pairs of column_name:value, with different columns separated by commas       
+        {     
         "value": data
         }
     ]
-}, default=str)
+    }, default=str)
     invoke_url = INVOKE_URL.format(topic_name=topic_name)
     requests.request("POST", invoke_url, headers=HEADERS, data=payload)
     
